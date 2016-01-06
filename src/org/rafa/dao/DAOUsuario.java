3ea748fdd6eac08d1a839e7890888979eb7ea063
elessar93 <rcardenas93@gmail.com>
2016-01-06 03:58:41 -0600

    dao

--- src/org/rafa/dao/DAOUsuario.java
+++ src/org/rafa/dao/DAOUsuario.java
@@ -1,5 +1,50 @@
 package org.rafa.dao;
 
-public class DAOUsuario {
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.rafa.dtd.Usuario;
 
+public class DAOUsuario {
+	Session session = null;
+	
+	public DAOUsuario() {
+		this.session = HibernateUtil.getSessionFactory().openSession();
+	}
+	
+	public boolean login(String nom_user,String password){
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(Usuario.class);
+		criteria.add(Restrictions.eq("nom_user", nom_user));
+		Usuario u = (Usuario)criteria.uniqueResult();
+		if(u!=null){
+			if(u.getPassword().equals(password))
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean registrar(Usuario u){
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(Usuario.class);
+		criteria.add(Restrictions.eq("nom_user", u.getNomUser()));
+		Usuario u1 = (Usuario)criteria.uniqueResult();
+		if(u1!=null)
+			return false;
+		try{
+			session.save(u);
+			session.getTransaction().commit();
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public Usuario usuarioById(int id){
+		Usuario u ;
+		session.beginTransaction();
+		u = (Usuario)session.get(Usuario.class, id);
+		return u;
+	}
 }
